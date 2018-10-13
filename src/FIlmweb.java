e3f1347efd982a8952b4f19d75b51f48fb9e7677
Damian Juzyk <damian.juzyk@gmail.com>
2018-10-13 10:48:16 +0200

    add clean code

--- src/FIlmweb.java
+++ src/FIlmweb.java
@@ -3,16 +3,12 @@ public class FIlmweb {
         //zmienna zawiera: typ, referencje, przypisanie
 
         //tworzenie tablicy: typ [] nazwa = new typ[rozmiar tablicy]
-        int x = 5;
-        int[] tab = {5, 10, 15};
-
 
         //tworzenie obiektu
         //Movie  movie1 = new Movie("Titanic", 1997, "James Cameron", "Dramat");
 
         Movie[] movies = new Movie[5];
         movies[0] =  new Movie("Titanic", 1997, "James Cameron", "Dramat");
-
        // movie1.show();
         movies[0].show();
         //wywowalenie metody na obiekcie
